d246def8337482133d885b76aff5a74da74f6ad0
Felipe Silva <felipe.dacs@gmail.com>
2020-01-20 10:32:26 -0300

    release: add aula 3

---
+++ main.js
@@ -0,0 +1,16 @@
+var nome = "Diego"
+var idade = 23
+var peso = 80.5
+var humano = true
+
+var alunos = [ 'Diego', 'Gabriel', 'Lucas' ]
+
+var aluno = {
+    nome: alunos[2],
+    idade: 23,
+    peso: 80.5,
+    humano: true
+}
+
+console.log(alunos[1])
+console.log(aluno.nome)
