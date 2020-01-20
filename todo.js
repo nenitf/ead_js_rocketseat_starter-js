09d797eca30bdb62b70681bc29c66f6bdfb147d4
Felipe Silva <felipe.dacs@gmail.com>
2020-01-20 13:57:34 -0300

    release: add aula 16

--- todo.js
+++ todo.js
@@ -1,1 +1,9 @@
-console.log("aaa")
+var listElement = document.querySelector("#app ul")
+var inputElement = document.querySelector("#app input")
+var buttonElement = document.querySelector("#app button")
+
+var todo = [
+    'Fazer café',
+    'Estudar JS',
+    'Acessar discord RS'
+]
