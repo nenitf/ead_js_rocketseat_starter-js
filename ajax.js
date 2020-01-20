8597d668c0e0aa84d0b232783c900bcb0e3f371c
Felipe Silva <felipe.dacs@gmail.com>
2020-01-20 15:01:10 -0300

    release: add aula 22

--- ajax.js
+++ ajax.js
@@ -1,11 +1,29 @@
-var xhr = new XMLHttpRequest()
+var minhaPromise = function(){
+    return new Promise(function(resolve, reject){
+        var xhr = new XMLHttpRequest()
 
-xhr.open('GET', 'https://api.github.com/users/nenitf')
-xhr.send(null)
+        xhr.open('GET', 'https://api.github.com/users/nenitf')
+        xhr.send(null)
 
-xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4){
-        console.log(JSON.parse(xhr.responseText))
-    }
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4){
+                if(xhr.status === 200){
+                    resolve(JSON.parse(xhr.responseText))
+                } else {
+                    reject('Erro na requisição')
+                }
+            }
+        }
+    })
 }
 
+// var resultado = minhaPromise()
+// console.log(resultado) // pending
+
+minhaPromise()
+    .then(function(response){
+        console.log(response)
+    })
+    .catch(function(error){
+        console.warn(error)
+    })
