846d13bb7c33c5d6257f5ba530019931ffa163ea
Felipe Silva <felipe.dacs@gmail.com>
2020-01-20 11:23:15 -0300

    release: add aula 9

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-// AND, OR, NOT
+// for e while
 
-var sexo = 'M'
+// interessante usar quando sabe o range limite
+for (var i = 0; i <= 10; i++){ // enquanto i for menor ou igual a 10, faça: 
+    console.log(i)
+}
 
-var resultado = (sexo === 'M') ? 'Masculino' : 'Feminino'
-
-console.log(resultado)
+// interessante usar quando não sabe o range limite, mas sim a condição
+var j = 500000500
+while(j > 5) { // Enquanto Jfor maior que 5, faça: 
+    console.log(j)
+    j /= 5
+}
