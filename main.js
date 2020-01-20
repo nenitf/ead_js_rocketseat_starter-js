f4dc705e3e2b2566066a20fee47c2043f0e05d5d
Felipe Silva <felipe.dacs@gmail.com>
2020-01-20 11:11:33 -0300

    release: add aula 8

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 // AND, OR, NOT
 
-var sexo = 'M', idade = 16
+var sexo = 'M'
 
-console.log(sexo === 'M' && idade >= 18) // true && false -> false
-console.log(sexo === 'M' || idade >= 18) // true || false -> true
-console.log(sexo === 'M') // true -> true
-console.log(sexo !== 'M') // ~(true) -> false
+var resultado = (sexo === 'M') ? 'Masculino' : 'Feminino'
+
+console.log(resultado)
