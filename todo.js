b2e2c63ff5dc6daea263669f1f27b4d9de9c002e
Felipe Silva <felipe.dacs@gmail.com>
2020-01-20 14:35:15 -0300

    release: add aula 19

--- todo.js
+++ todo.js
@@ -10,10 +10,23 @@ var todos = [
 
 function renderTodos() {
     listElement.innerHTML = ''
+
     for (todo of todos) {
         var todoElement = document.createElement('li')
         var todoText = document.createTextNode(todo)
+
+        var linkElement = document.createElement('a')
+        linkElement.setAttribute('href', '#')
+
+        var pos = todos.indexOf(todo)
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')')
+
+        var linkText = document.createTextNode('Excluir')
+        linkElement.appendChild(linkText)
+
         todoElement.appendChild(todoText)
+        todoElement.appendChild(linkElement)
+
         listElement.appendChild(todoElement)
     }
 }
@@ -29,3 +42,8 @@ function addTodo(){
 }
 
 buttonElement.onclick = addTodo
+
+function deleteTodo(pos){
+    todos.splice(pos,1)
+    renderTodos()
+}
