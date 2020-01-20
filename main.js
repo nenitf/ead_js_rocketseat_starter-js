10062dbfac109768d5310baa2f05acf21411fc8e
Felipe Silva <felipe.dacs@gmail.com>
2020-01-20 11:40:02 -0300

    release: add aula 12

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-function mostraAlerta(evento){
-    console.log(`Evento ${evento} acionado`)
+console.log(document.getElementById('btn')) // tag
+console.log(document.getElementsByTagName('button')[0]) // HTMLCollections[0]
+
+console.log(document.querySelector('body button'))
+console.log(document.querySelector('button'))
+console.log(document.querySelector('#btn'))
+
+document.querySelector('#btn').onclick = function() {
+    console.log(document.querySelector('input').value)
 }
