2b51492e3b475bc77e348b4afb7802576c79a7df
Felipe Silva <felipe.dacs@gmail.com>
2020-01-20 11:53:44 -0300

    release: add aula 14

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
-var link = document.createElement('a')
-link.setAttribute('href', 'http://neni.dev')
-link.setAttribute('title', 'portfólio dev')
+var boxElement = document.querySelector(".box")
 
-var text = document.createTextNode('Acessar meu site')
-link.appendChild(text)
-
-// add link ao body
-var container = document.querySelector('body')
-container.appendChild(link)
-
-// remove btn do body
-var btn = document.querySelector('#btn')
-container.removeChild(btn)
+boxElement.style.width = 100;
+boxElement.style.height = 100;
+boxElement.style.backgroundColor = 'purple';
