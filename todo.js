c4e21650fc440d000819f78d457e415be409ec87
Felipe Silva <felipe.dacs@gmail.com>
2020-01-20 14:03:20 -0300

    release: add aula 17

--- todo.js
+++ todo.js
@@ -2,8 +2,19 @@ var listElement = document.querySelector("#app ul")
 var inputElement = document.querySelector("#app input")
 var buttonElement = document.querySelector("#app button")
 
-var todo = [
+var todos = [
     'Fazer café',
     'Estudar JS',
     'Acessar discord RS'
 ]
+
+function renderTodos() {
+    for (todo of todos) {
+        var todoElement = document.createElement('li')
+        var todoText = document.createTextNode(todo)
+        todoElement.appendChild(todoText)
+        listElement.appendChild(todoElement)
+    }
+}
+
+renderTodos()
